0eee641c23aefba435af212764c16dee4b34a4ae
ishabaral <baralisha.16@gmail.com>
2021-07-06 16:09:21 +0545

    input readonly

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const reset = () => {
     document.getElementById("input-display").value = "";
 }
 
-const partialReset = () => {
-    document.getElementById("input-box").value = "";
+const clearOne = () => {
+    let inputValue = document.getElementById("input-box").value;
+    let modifiedInput = inputValue.substring(0, inputValue.length - 1);
+    document.getElementById("input-box").value = modifiedInput;
 }
 
 const displayNum = (num) => document.getElementById("input-box").value += num;
 
-const result = () => {
-    let a = document.getElementById("input-box").value;
-    let b = eval(a);
-    document.getElementById("input-box").value = b;
-    document.getElementById("input-display").value = a;
+
+const displayResult = () => {
+    let expression = document.getElementById("input-box").value;
+    let result = eval(expression);
+    document.getElementById("input-box").value = result;
+    document.getElementById("input-display").value = expression;
 }
